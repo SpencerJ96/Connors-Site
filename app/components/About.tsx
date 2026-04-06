8ea4c16e8608b17082eff22ba9abe0341452f1f1
SpencerJ96 <spencer-jones1@hotmail.co.uk>
2026-04-06 22:07:39 +0100

    Added about section with stats counting up function - Added social icons with picture

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -1,36 +1,78 @@
+"use client"
+import { useInView } from "framer-motion"
+import { useRef, useState, useEffect } from "react"
 import Image from "next/image"
+import { FaInstagram, FaYoutube, FaTiktok } from "react-icons/fa"
+
+
+function CountUp ( { target, isInView, step = 1 } : { target: number, isInView: boolean, step? : number}) {
+	const [count, setCount] = useState(0)
+
+	useEffect(() => {
+		if (isInView) {
+			const interval = setInterval(() => {
+				setCount(prev =>{
+					if (prev >= target) {
+						clearInterval(interval)
+						return target
+					}
+					return prev + step
+				})
+			},20)
+		}
+	}, [isInView])
+		return <span>{count}</span>
+
+}
+
+
+
 
 export default function About(){
+	const ref = useRef(null)
+	const isInView = useInView(ref, { once: true })
+
+
 	return(
-		<section id="about" className="py-14 px-8">
+		<section id="about" className="py-14 px-16">
 		
-<div className="grid grid-cols-2 gap-16">
+<div className="grid grid-cols-2 gap-16  mx-auto">
+<div className="flex flex-col items-center justify-self-center gap-4">
+	
+	<div className="relative h-100 w-100 rounded-full overflow-hidden ring-4 ring-accent">
+		<Image src="/ConnorImg.jpg"  alt="placeholder img" fill className="object-cover"/>
+	</div>
+	<div className="flex gap-4 justify-center">
+    <a href="#" className="text-3xl hover:text-accent transition-colors duration-200"><FaInstagram /></a>
+    <a href="#" className="text-3xl hover:text-accent transition-colors duration-200"><FaTiktok /></a>
+    <a href="#" className="text-3xl hover:text-accent transition-colors duration-200"><FaYoutube /></a>
+  </div> 
+</div>
+		<div className="flex flex-col items-center pl-1"> 
+			<h2 className="text-accent text-5xl font-bold mb-4">About</h2>
+			<div className="w-16 h-0.5 bg-accent mb-6"></div>
+			<p className="py-4 text-2xl text-foreground/70 leading-relaxed">Connor Alexander is a Nottingham-based videographer and content creator with a passion for visual storytelling. Graduating with a BA in Content Creation, Connor brings a creative eye and technical precision to every project — from brand campaigns to personal showreels. His work spans commercial, lifestyle, and event videography, crafting narratives that connect and resonate.</p>
 		
-		<div className="relative h-96 w-96 rounded-full overflow-hidden ring-4 ring-accent">
-		<Image src="/connoraboutimg.jpg"  alt="placeholder img" fill className="object-cover"/>
-		</div>
-
-		<div className="flex flex-col items-center"> 
-			<h2 className="text-4xl font-bold mb-4">About</h2>
-			<p className="text-foreground/70 leading-relaxed">Connor Alexander is a Nottingham-based videographer and content creator with a passion for visual storytelling. Graduating with a BA in Content Creation, Connor brings a creative eye and technical precision to every project — from brand campaigns to personal showreels. His work spans commercial, lifestyle, and event videography, crafting narratives that connect and resonate.</p>
-		</div>
-
-		<div className="flex gap-8 mt-8">
+		<div ref={ref}className="flex gap-8 mt-8 text-center bg-foreground/5 rounded-2xl px-8 py-6">
 			<div>
-				<p className="text-3xl fontbold">28</p>
-				<p className="text-sm text-foreground/60">Projects Completed</p>
+				<p className="text-accent text-4xl font-bold mb-2"> <CountUp target={28} isInView={isInView} /> </p>
+				<p className="uppercase tracking-widest text-xs text-foreground/60 border-l border-foreground/20 pl-8">Projects Completed</p>
 			</div>
 
 			<div>
-				<p className="text-3xl fontbold">1,584</p>
-				<p className="text-sm text-foreground/60">Instagram Likes</p>
+				<p className="text-accent text-4xl font-bold mb-2"> <CountUp target={1584} isInView={isInView} step={12} /> </p>
+				<p className="uppercase tracking-widest text-xs text-foreground/60 border-l border-foreground/20 pl-8">Instagram Likes</p>
 			</div>
 
 			<div>
-				<p className="text-3xl fontbold">BA</p>
-				<p className="text-sm text-foreground/60">Content Creation</p>
+				<p className="text-accent text-4xl font-bold mb-2">BA</p>
+				<p className="uppercase tracking-widest text-xs text-foreground/60 border-l border-foreground/20 pl-8">Content Creation</p>
 			</div>
 		</div>
+		
+		</div>
+
+		
 </div>
 		</section>
 	)
